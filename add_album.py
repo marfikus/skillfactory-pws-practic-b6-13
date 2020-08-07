06c3f6a1ba4fd21291a2d2500f0ffc3ff21c872e
Alexey Zaytsev <alex.rv9rh@gmail.com>
2020-08-07 14:51:44 +0700

    Add validate of input data

--- add_album.py
+++ add_album.py
@@ -26,7 +26,37 @@ def connect_db():
     session = Sessions()
     return session
     
-
+def valid_input_data(album):
+    MAX_LENGTH = 150
+    
+    year = str(album["year"])
+    if len(year) < 4:
+        return "Length of field 'Year' less than 4 characters"
+    if len(year) > 4:
+        return "Length of field 'Year' more than 4 characters"
+    try:
+        year = int(year)
+    except ValueError:
+        return "Year is not a number"
+    if year < 0:
+        return "Year is negative number"
+   
+    if album["artist"].strip() == "":
+        return "Field 'Artist' is empty"
+    if len(album["artist"]) > MAX_LENGTH:
+        return "Length of field 'Artist' more than {} characters".format(MAX_LENGTH)
+        
+    if album["genre"].strip() == "":
+        return "Field 'Genre' is empty"
+    if len(album["genre"]) > MAX_LENGTH:
+        return "Length of field 'Genre' more than {} characters".format(MAX_LENGTH)
+        
+    if album["album"].strip() == "":
+        return "Field 'Album' is empty"
+    if len(album["album"]) > MAX_LENGTH:
+        return "Length of field 'Album' more than {} characters".format(MAX_LENGTH)
+    
+    return ""
     
 def find(artist):
     session = connect_db()
